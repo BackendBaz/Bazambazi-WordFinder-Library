2dd5981c1cf9e6e3000a19a5a0fc61ba07a91de7
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-24 01:38:17 -0700

    feat: implement the array of letters in the getLetters method (it passed 3 tests)

--- src/main/java/io/github/backendbaz/core/Finder.java
+++ src/main/java/io/github/backendbaz/core/Finder.java
@@ -2,6 +2,8 @@ package io.github.backendbaz.core;
 
 import io.github.backendbaz.exceptions.InvalidLettersException;
 
+import java.util.Arrays;
+
 public class Finder {
 
     private int rows = 4;
@@ -31,6 +33,10 @@ public class Finder {
     public String[][] getLetters() throws InvalidLettersException {
         if (input == null || input.isBlank())
             throw new InvalidLettersException("Input is null or blank");
+        String[] letters = input.split(" ");
+        if (letters.length != rows * cols || !Arrays.stream(letters)
+                .allMatch(letter -> letter.matches("[ا-ی]")))
+            throw new InvalidLettersException("Some letters are not valid");
         return new String[][]{};
     }
 
--- src/test/java/io/github/backendbaz/core/FinderTest.java
+++ src/test/java/io/github/backendbaz/core/FinderTest.java
@@ -26,4 +26,34 @@ public class FinderTest {
                 .hasMessageContaining("Input is null or blank");
     }
 
+    @Test
+    @Tag("Finder:getLetters")
+    @DisplayName("Throws the InvalidLettersException when the letters array's length is invalid")
+    public void getLetters_WhenLettersArrayLengthIsInvalid_ShouldThrowsInvalidLettersException() {
+        assertThatThrownBy(() ->
+                new Finder("س ع ق ف ا").getLetters())
+                .isInstanceOf(InvalidLettersException.class)
+                .hasMessageContaining("Some letters are not valid");
+    }
+
+    @Test
+    @Tag("Finder:getLetters")
+    @DisplayName("Throws the InvalidLettersException when the letters array is invalid")
+    public void getLetters_WhenLettersArrayIsInvalid_ShouldThrowsInvalidLettersException() {
+        assertThatThrownBy(() ->
+                new Finder("س ع ق ف ا ب ف ظ a ل ذ ل ب س ش ا").getLetters())
+                .isInstanceOf(InvalidLettersException.class)
+                .hasMessageContaining("Some letters are not valid");
+    }
+
+    @Test
+    @Tag("Finder:getLetters")
+    @DisplayName("Throws the InvalidLettersException when the Persian uppercase 'a' letter exists in the letters array")
+    public void getLetters_WhenPersianUppercaseAExistsInTheLettersArray_ShouldThrowsInvalidLettersException() {
+        assertThatThrownBy(() ->
+                new Finder("س ع ق ف ا ب ف ظ آ ل ذ ل ب س ش ا").getLetters())
+                .isInstanceOf(InvalidLettersException.class)
+                .hasMessageContaining("Some letters are not valid");
+    }
+
 }
