03e0ce907589ea9bfd563184fc254a5e29b0ce62
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-23 21:23:12 -0700

    feat: implement welcome message and user input with while loop and try-catch error management

--- src/main/java/io/github/backendbaz/Main.java
+++ src/main/java/io/github/backendbaz/Main.java
@@ -1,9 +1,27 @@
 package io.github.backendbaz;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Bazambazi Word Finder CLI!");
+        while (true) {
+            try {
+                System.out.println("Enter your Persian letters separated by a space " +
+                        "or insert 'e' to exit the program:");
+                Scanner scan = new Scanner(System.in);
+                String letters = scan.nextLine();
+                if (letters.equalsIgnoreCase("e")) {
+                    System.out.println("\nBye!");
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Something went wrong! Try again.");
+            } finally {
+                System.out.println("=".repeat(50));
+            }
+        }
     }
 
 }
