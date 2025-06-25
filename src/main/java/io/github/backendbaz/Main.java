b57e45324e371a4f7203e8a1ff3b3691f2ae2ae8
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-25 08:49:26 -0700

    feat: implement the Main method to return 3 words in the grid with point

--- src/main/java/io/github/backendbaz/Main.java
+++ src/main/java/io/github/backendbaz/Main.java
@@ -5,8 +5,8 @@ import io.github.backendbaz.core.Finder;
 import io.github.backendbaz.core.Word;
 import io.github.backendbaz.exceptions.InvalidLettersException;
 import org.json.simple.parser.ParseException;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,9 +18,7 @@ public class Main implements Runnable {
         Main main = new Main();
         Thread thread = new Thread(main);
         thread.start();
-        while (thread.isAlive()) {
-            System.out.println("loading...");
-        }
+        while (thread.isAlive()) System.out.println("loading...");
         runApplication();
     }
 
@@ -38,7 +36,15 @@ public class Main implements Runnable {
                 }
                 Finder finder = new Finder(letters);
                 String[][] gridOfLetters = finder.getLetters();
-                System.out.println(words.getFirst().getWord()); // todo
+                List<Word> results = new ArrayList<>();
+                for (Word word : words) {
+                    if (finder.exists(gridOfLetters, word.getWord()))
+                        results.add(word);
+                    if (results.size() >= 3) break;
+                }
+                results.forEach(word ->
+                        System.out.println("Word: " + word.getWord() +
+                                " - Point: " + word.getPoint()));
             } catch (InvalidLettersException e) {
                 System.out.println("Invalid letters Error -> " + e.getMessage());
             } catch (Exception e) {
